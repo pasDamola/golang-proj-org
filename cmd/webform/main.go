cb69d70079fbb3b5b10997e1667dc40fcd04d878
oyinda <oyindadavid18@gmail.com>
2025-05-14 16:46:11 +0100

    Importing third party packages

--- cmd/webform/main.go
+++ cmd/webform/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/PacktPublishing/Go-Recipes-for-Developers/src/chp1/webform/pkg/commentdb"
+	_ "modernc.org/sqlite"
+)
 
 func main() {
+	if db, err := sql.Open("sqlite", "webform.db"); err != nil {
+		panic(err)
+	}
+
+	commentdb.InitDB(db)
+
 	server := http.Server{
 		Addr:    ":8181",
 		Handler: http.FileServer(http.Dir("web/static")),
 	}
+
 	server.ListenAndServe()
 }
